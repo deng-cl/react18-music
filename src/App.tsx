1defa85b830b9b3a33977a358bdd66f621276282
2180733045@qq.com <13423027+deng--cl@user.noreply.gitee.com>
2024-07-02 02:39:33 +0800

    React18 + TS: coderkxh-music 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import { memo } from 'react'
+
+const App = memo(() => {
+    return (
+        <div className="App">
+            <h2>coderkxh-music</h2>
+        </div>
+    )
+})
+
+
+export default App
