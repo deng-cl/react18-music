d32b154b277fcc574afb38a74b19308268e01ef3
2180733045@qq.com <13423027+deng--cl@user.noreply.gitee.com>
2024-07-13 17:41:57 +0800

    优化部分代码 --> coderkxh-music: version 1 结束

--- src/App.tsx
+++ src/App.tsx
@@ -11,32 +11,41 @@ import { appShallowEqual, useAppDispatch, useAppSelector } from "./store/app-rea
 import { Spin } from "antd"
 import { ThemeProvider } from "styled-components"
 import THEME from "./assets/theme"
+import { changeShowPlayListAction, changeShowVolumeControlAction } from "./views/player/store/module/audio-operator"
 
 export const AppContext = createContext({}) // -- 通过 React 中的 Context 进行对应函数的传递
 
 const App = memo(() => {
     const pagename = usePageName() // -- 获取当前页面名称
 
+    const dispatch = useAppDispatch()
     const location = useLocation()
 
     const NOT_FOOTER_ROUTE_PATHS: IRoutesPath[] = ["/theme"] // -- 配置哪些页面不需要显示 footer 页面
 
-    const { loading, isDark } = useAppSelector(state => ({
+    const { loading, isDark, showVolumeControl, showPlayList } = useAppSelector(state => ({
         loading: state.main.loading,
-        isDark: state.theme.isDark
+        isDark: state.theme.isDark,
+        showVolumeControl: state.audioOperator.showVolumeControl,
+        showPlayList: state.audioOperator.showPlayList
     }), appShallowEqual)
 
     // -- ↓ 为后代元素注入 pageRef 对象 --> 使其可以操作该 page 元素（目前主要用于操作滚动太到顶部）
     const pageRef = useRef<HTMLElement>()
 
+    const handlePlayerBarContent = () => {  // -- 点击非 PlayerBar 页面部分，取消显示对应音量控件与歌曲列表展示部分
+        if (showVolumeControl) dispatch(changeShowVolumeControlAction(false))
+        if (showPlayList) dispatch(changeShowPlayListAction(false))
+    }
+
     return (
         <ThemeProvider theme={isDark ? THEME.dark : THEME.light}>
             <AppWrapper className="App">
-                <div className="main-left">
+                <div className="main-left" onClick={handlePlayerBarContent}>
                     <AppNavlist />
                 </div>
 
-                <div className="main-right" ref={pageRef as any}>
+                <div className="main-right" ref={pageRef as any} onClick={handlePlayerBarContent}>
                     <AppHeader />
                     <div className="show-page-name">{pagename}</div>
                     <div className="content">
